6862dc19809f78c0d7abad8d8c72a0e2d0bc5b7d
João Guilherme B. do Nascimento <joaoguilhermebn@hotmail.com>
2023-01-23 23:02:42 -0300

    implementing websockets

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,37 @@
 import express from 'express';
 import path from 'node:path';
+import http from 'node:http';
 import mongoose from 'mongoose';
+import { Server } from 'socket.io';
+
+
 import { router } from './router';
 
 mongoose.connect('mongodb://localhost:27017')
 	.then(() => {
-		const app = express();
 		const port = 3001;
 
+
+
+		const app = express();
+		const server = http.createServer(app);
+		const io = new Server(server);
+
+		
+
+
+		app.use((req, res, next) => {
+			res.setHeader('Access-Control-Allow-Origin', '*');
+			res.setHeader('Access-Control-Allow-Methods', '*');
+			res.setHeader('Access-Control-Allow-Headers', '*');
+
+			next();
+		});
 		app.use('/uploads', express.static(path.resolve(__dirname, '..','uploads')));
 		app.use(express.json());
 		app.use(router);
 
-		app.listen(port, () => {
+		server.listen(port, () => {
 			console.log(`🚀 Server is running on http://localhost:${port}`);
 		});
 	})
