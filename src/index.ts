955b29dcb261a78a219fb5b2e239a5894ea8dafb
João Guilherme B. do Nascimento <joaoguilhermebn@hotmail.com>
2023-01-28 14:17:56 -0300

    Waiter app done!

--- src/index.ts
+++ src/index.ts
@@ -7,17 +7,16 @@ import { Server } from 'socket.io';
 
 import { router } from './router';
 
+
+const app = express();
+const server = http.createServer(app);
+export const io = new Server(server);
+
 mongoose.connect('mongodb://localhost:27017')
 	.then(() => {
 		const port = 3001;
 
 
-
-		const app = express();
-		const server = http.createServer(app);
-		const io = new Server(server);
-
-		
 
 
 		app.use((req, res, next) => {
@@ -27,6 +26,8 @@ mongoose.connect('mongodb://localhost:27017')
 
 			next();
 		});
+
+
 		app.use('/uploads', express.static(path.resolve(__dirname, '..','uploads')));
 		app.use(express.json());
 		app.use(router);
